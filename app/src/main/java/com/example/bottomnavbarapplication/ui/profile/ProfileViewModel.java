252b25dfdd7f56c29990676253ef71efee8f1bb5
Robertwentzell <robwentz98@hotmail.com>
2019-12-07 11:27:01 -0500

    profile implementation

--- app/src/main/java/com/example/bottomnavbarapplication/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/bottomnavbarapplication/ui/profile/ProfileViewModel.java
@@ -8,10 +8,6 @@ public class ProfileViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public ProfileViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is profile fragment");
-    }
 
     public LiveData<String> getText() {
         return mText;
